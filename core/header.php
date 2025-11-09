61f2eed04c0f58525f02ee10e1ad51fa3fb278fa
Kadir YAPAR <yaparkadir@gmail.com>
2025-11-09 15:17:22 +0300

    Update header.php

--- core/header.php
+++ core/header.php
@@ -14,7 +14,7 @@
             content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>BEAM Unbound DNS Server</title>
+    <title>Unbound DNS Server</title>
 
     <meta name="description" content=""/>
 
