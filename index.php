e21155a368ee80282d138c52fb6c389457e8d4ca
Kadir YAPAR <yaparkadir@gmail.com>
2025-11-09 15:19:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
 
-    <title>BEAM DNS SERVER</title>
+    <title>UNBOUND DNS SERVER</title>
 
     <meta name="description" content=""/>
 
